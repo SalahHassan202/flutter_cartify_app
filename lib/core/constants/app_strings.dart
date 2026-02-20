e19c470ecf0ec65b5fc58768e6fe7ca250d4c28d
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-21 00:09:27 +0200

    added app strings file

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,14 @@
+class AppStrings {
+  AppStrings._();
+
+  static const appName = "Cartify";
+  static const tagline = "Shop Smart. Live Better.";
+
+  static const login = "Sign In";
+  static const register = "Create Account";
+  static const email = "Email Address";
+  static const password = "Password";
+
+  static const addToCart = "Add to Cart";
+  static const checkout = "Checkout";
+}
