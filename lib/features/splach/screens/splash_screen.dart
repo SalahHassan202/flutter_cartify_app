3b44547fb08a8c906de5b0d64648280536b201c9
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-21 01:52:43 +0200

    finish splach screen

---
+++ lib/features/splach/screens/splash_screen.dart
@@ -0,0 +1,141 @@
+import 'package:cartify_app/features/onboarding/screens/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../core/constants/app_assets.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_text_styles.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _mainController;
+  late AnimationController _wheelController;
+
+  late Animation<double> _scale;
+  late Animation<double> _fade;
+  late Animation<Offset> _slide;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _mainController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _wheelController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _scale = CurvedAnimation(
+      parent: _mainController,
+      curve: Curves.easeOutBack,
+    );
+
+    _fade = CurvedAnimation(parent: _mainController, curve: Curves.easeIn);
+
+    _slide = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _mainController, curve: Curves.easeOut));
+
+    _mainController.forward();
+    _wheelController.repeat();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(builder: (_) => const OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _mainController.dispose();
+    _wheelController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppColors.lightBackground, Color(0xFFF5F7FA)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: FadeTransition(
+            opacity: _fade,
+            child: SlideTransition(
+              position: _slide,
+              child: ScaleTransition(
+                scale: _scale,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    /// 🛒 Logo + Wheel
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Image.asset(AppAssets.logo, width: 140.w),
+
+                        Positioned(
+                          bottom: 10.h,
+                          right: 18.w,
+                          child: RotationTransition(
+                            turns: _wheelController,
+                            child: Container(
+                              width: 14.w,
+                              height: 14.w,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: AppColors.accent,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 28.h),
+
+                    /// App Name
+                    Text(
+                      "Cartify",
+                      style: AppTextStyles.heading1.copyWith(
+                        fontSize: 30.sp,
+                        color: AppColors.primary,
+                      ),
+                    ),
+
+                    SizedBox(height: 8.h),
+
+                    /// Tagline
+                    Text(
+                      "Shop Smart. Live Better.",
+                      style: AppTextStyles.caption.copyWith(fontSize: 14.sp),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
