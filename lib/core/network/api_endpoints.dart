8972ada1c337e4841f439a4e360fbd51f40c405d
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-21 00:51:38 +0200

    added api endpoints file

---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,8 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const String baseUrl = "https://fakestoreapi.com";
+
+  static const String products = "/products";
+  static const String categories = "/products/categories";
+}
