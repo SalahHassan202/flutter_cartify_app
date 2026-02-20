beb0da7810834111d5870046e45e41b4b30dba33
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-21 00:14:19 +0200

    added app assets file

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,17 @@
+class AppAssets {
+  AppAssets._();
+
+  // Logo
+  static const logo = "assets/images/logo/logo.png";
+
+  // Onboarding
+  static const onboarding1 = "assets/images/onboarding/1.png";
+  static const onboarding2 = "assets/images/onboarding/2.png";
+  static const onboarding3 = "assets/images/onboarding/3.png";
+
+  // Home
+  static const homeBanner = "assets/images/home/banner.png";
+
+  // Profile
+  static const profileImage = "assets/images/profile/profile.png";
+}
