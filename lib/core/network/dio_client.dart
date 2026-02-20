478864c7841d44bcf4198b896532115b69c5864c
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-21 00:52:33 +0200

    added dio client file

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'api_endpoints.dart';
+
+class DioClient {
+  DioClient._();
+
+  static final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: ApiEndpoints.baseUrl,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+    ),
+  );
+}
