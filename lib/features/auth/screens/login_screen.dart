10c7c4c6a3e67a4e6139ad9f98338c9d5c357e88
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-22 06:25:27 +0200

    update login screen file

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/constants/app_assets.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_spacing.dart';
+import '../../../core/constants/app_text_styles.dart';
+import '../../../core/utils/validators.dart';
+import '../cubit/auth_cubit.dart';
+import '../cubit/auth_state.dart';
+import '../widgets/auth_text_field.dart';
+import '../widgets/auth_button.dart';
+import 'register_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => AuthCubit(),
+      child: Scaffold(
+        backgroundColor: AppColors.lightBackground,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: AppSpacing.s24.w),
+            child: SingleChildScrollView(
+              child: BlocListener<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthSuccess) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Login Successful")),
+                    );
+                  } else if (state is AuthFailure) {
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text(state.message)));
+                  }
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 40.h),
+
+                    Center(
+                      child: Image.asset(AppAssets.loginImage, height: 220.h),
+                    ),
+
+                    SizedBox(height: 30.h),
+
+                    Text("Welcome Back", style: AppTextStyles.heading1),
+
+                    SizedBox(height: 8.h),
+
+                    Text(
+                      "Login to continue your journey.",
+                      style: AppTextStyles.body,
+                    ),
+
+                    SizedBox(height: 30.h),
+
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          AuthTextField(
+                            controller: emailController,
+                            label: "Email",
+                            keyboardType: TextInputType.emailAddress,
+                            validator: Validators.validateEmail,
+                          ),
+
+                          SizedBox(height: AppSpacing.s16.h),
+
+                          AuthTextField(
+                            controller: passwordController,
+                            label: "Password",
+                            obscureText: true,
+                            validator: Validators.validatePassword,
+                          ),
+
+                          SizedBox(height: 30.h),
+
+                          BlocBuilder<AuthCubit, AuthState>(
+                            builder: (context, state) {
+                              return AuthButton(
+                                text: "Login",
+                                isLoading: state is AuthLoading,
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    context.read<AuthCubit>().login(
+                                      email: emailController.text.trim(),
+                                      password: passwordController.text.trim(),
+                                    );
+                                  }
+                                },
+                              );
+                            },
+                          ),
+
+                          SizedBox(height: 20.h),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text("Don't have an account? "),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => RegisterScreen(),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  "Register",
+                                  style: TextStyle(
+                                    color: AppColors.primary,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
