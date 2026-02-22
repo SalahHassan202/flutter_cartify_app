49ca5568e8fd50ffab9f279bc6e42ddb8e03ad3c
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-22 13:39:52 +0200

    try to print error message to solve error in code

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -15,7 +15,8 @@ class HomeCubit extends Cubit<HomeState> {
       final products = await api.getProducts();
       emit(HomeSuccess(products));
     } catch (e) {
-      emit(HomeError("Failed to load products"));
+      print(e.toString());
+      emit(HomeError(e.toString()));
     }
   }
 }
