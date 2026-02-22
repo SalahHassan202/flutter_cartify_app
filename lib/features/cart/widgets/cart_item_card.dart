2f6f5a0d31fbd7c0562775be8dbd624b8363937b
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-23 00:25:41 +0200

    update cart item card widget

--- lib/features/cart/widgets/cart_item_card.dart
+++ lib/features/cart/widgets/cart_item_card.dart
@@ -1,4 +1,6 @@
+import 'package:cartify_app/features/cart/cubit/cart_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_text_styles.dart';
@@ -6,13 +8,8 @@ import '../../home/models/product_model.dart';
 
 class CartItemCard extends StatelessWidget {
   final ProductModel product;
-  final VoidCallback onRemove;
 
-  const CartItemCard({
-    super.key,
-    required this.product,
-    required this.onRemove,
-  });
+  const CartItemCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +49,7 @@ class CartItemCard extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                   maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
                 ),
                 SizedBox(height: 4.h),
                 Text(
@@ -64,11 +62,43 @@ class CartItemCard extends StatelessWidget {
               ],
             ),
           ),
-          IconButton(
-            icon: const Icon(Icons.delete_outline, color: Colors.red),
-            onPressed: onRemove,
+          Row(
+            children: [
+              _qtyBtn(
+                Icons.remove,
+                () => context.read<CartCubit>().removeOneFromCart(product),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: Text(
+                  "${product.quantity}",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              _qtyBtn(
+                Icons.add,
+                () => context.read<CartCubit>().addToCart(product),
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _qtyBtn(IconData icon, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(6.w),
+        decoration: BoxDecoration(
+          color: AppColors.primary.withValues(alpha: 0.1),
+          borderRadius: BorderRadius.circular(8.r),
+        ),
+        child: Icon(icon, size: 18.sp, color: AppColors.primary),
       ),
     );
   }
