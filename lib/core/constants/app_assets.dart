ff27398f4f272233330cf0296fd0213669f436f7
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-22 06:15:55 +0200

    update app assets file

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,17 +1,16 @@
 class AppAssets {
   AppAssets._();
 
-  // Logo
   static const logo = "assets/images/logo/logo.png";
 
-  // Onboarding
   static const onboarding1 = "assets/images/onboarding/1.png";
   static const onboarding2 = "assets/images/onboarding/2.png";
   static const onboarding3 = "assets/images/onboarding/3.png";
 
-  // Home
+  static const loginImage = "assets/images/auth/login.png";
+  static const registerImage = "assets/images/auth/register.png";
+
   static const homeBanner = "assets/images/home/banner.png";
 
-  // Profile
   static const profileImage = "assets/images/profile/profile.png";
 }
