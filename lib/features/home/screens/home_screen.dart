4e0c0e2e54c400dcbfc418b048f00de42195a083
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-22 13:24:21 +0200

    update home screen file

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../core/constants/app_colors.dart';
+import '../cubit/home_cubit.dart';
+import '../cubit/home_state.dart';
 import '../widgets/home_app_bar.dart';
 import '../widgets/home_search_bar.dart';
 import '../widgets/home_banner.dart';
@@ -13,36 +16,55 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.lightBackground,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 16.h),
+    return BlocProvider(
+      create: (_) => HomeCubit()..fetchProducts(),
+      child: Scaffold(
+        backgroundColor: AppColors.lightBackground,
+        body: SafeArea(
+          child: BlocBuilder<HomeCubit, HomeState>(
+            builder: (context, state) {
+              if (state is HomeLoading) {
+                return const Center(child: CircularProgressIndicator());
+              }
 
-              const HomeAppBar(),
+              if (state is HomeError) {
+                return Center(child: Text(state.message));
+              }
 
-              SizedBox(height: 20.h),
+              if (state is HomeSuccess) {
+                return SingleChildScrollView(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 16.h),
 
-              const HomeSearchBar(),
+                      const HomeAppBar(),
 
-              SizedBox(height: 20.h),
+                      SizedBox(height: 20.h),
 
-              const HomeBanner(),
+                      const HomeSearchBar(),
 
-              SizedBox(height: 24.h),
+                      SizedBox(height: 20.h),
 
-              const CategoryList(),
+                      const HomeBanner(),
 
-              SizedBox(height: 24.h),
+                      SizedBox(height: 24.h),
 
-              const ProductGrid(),
+                      const CategoryList(),
 
-              SizedBox(height: 40.h),
-            ],
+                      SizedBox(height: 24.h),
+
+                      ProductGrid(products: state.products),
+
+                      SizedBox(height: 40.h),
+                    ],
+                  ),
+                );
+              }
+
+              return const SizedBox();
+            },
           ),
         ),
       ),
