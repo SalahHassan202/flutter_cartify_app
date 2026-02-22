af4b563f6542aa78aa6a5ba64e8a6f0e2ae861c3
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-22 23:36:38 +0200

    update main layout screen

--- lib/features/main_layout/screens/main_layout_screen.dart
+++ lib/features/main_layout/screens/main_layout_screen.dart
@@ -15,70 +15,115 @@ class MainLayoutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final pages = [
+      const HomeScreen(),
+      const FavoritesScreen(),
+      const CartScreen(),
+      const ProfileScreen(),
+    ];
+
     return BlocProvider(
       create: (_) => LayoutCubit(),
       child: BlocBuilder<LayoutCubit, LayoutState>(
         builder: (context, state) {
           final cubit = context.read<LayoutCubit>();
 
-          final pages = [
-            const HomeScreen(),
-            const FavoritesScreen(),
-            const CartScreen(),
-            const ProfileScreen(),
-          ];
-
           return Scaffold(
+            extendBody: true,
             body: pages[cubit.currentIndex],
             bottomNavigationBar: Container(
+              height: 70.h,
+              margin: EdgeInsets.all(20.w),
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(24.r)),
+                borderRadius: BorderRadius.circular(30.r),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 20,
-                    offset: const Offset(0, -4),
+                    color: Colors.black.withValues(alpha: 0.15),
+                    blurRadius: 25,
+                    offset: const Offset(0, 10),
                   ),
                 ],
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(24.r)),
-                child: BottomNavigationBar(
-                  currentIndex: cubit.currentIndex,
-                  onTap: cubit.changeTab,
-                  type: BottomNavigationBarType.fixed,
-                  selectedItemColor: AppColors.primary,
-                  unselectedItemColor: Colors.grey,
-                  showSelectedLabels: true,
-                  showUnselectedLabels: false,
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home_outlined),
-                      activeIcon: Icon(Icons.home),
-                      label: "Home",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.favorite_border),
-                      activeIcon: Icon(Icons.favorite),
-                      label: "Favorites",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.shopping_cart_outlined),
-                      activeIcon: Icon(Icons.shopping_cart),
-                      label: "Cart",
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.person_outline),
-                      activeIcon: Icon(Icons.person),
-                      label: "Profile",
-                    ),
-                  ],
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildNavItem(
+                    context,
+                    0,
+                    Icons.home_rounded,
+                    "Home",
+                    cubit.currentIndex,
+                  ),
+                  _buildNavItem(
+                    context,
+                    1,
+                    Icons.favorite_rounded,
+                    "Favorites",
+                    cubit.currentIndex,
+                  ),
+                  _buildNavItem(
+                    context,
+                    2,
+                    Icons.shopping_cart_rounded,
+                    "Cart",
+                    cubit.currentIndex,
+                  ),
+                  _buildNavItem(
+                    context,
+                    3,
+                    Icons.person_rounded,
+                    "Profile",
+                    cubit.currentIndex,
+                  ),
+                ],
               ),
             ),
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildNavItem(
+    BuildContext context,
+    int index,
+    IconData icon,
+    String label,
+    int currentIndex,
+  ) {
+    bool isSelected = currentIndex == index;
+    return GestureDetector(
+      onTap: () => context.read<LayoutCubit>().changeTab(index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
+        decoration: BoxDecoration(
+          color: isSelected
+              ? AppColors.primary.withValues(alpha: 0.1)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(20.r),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? AppColors.primary : Colors.grey,
+              size: 26.sp,
+            ),
+            if (isSelected) SizedBox(width: 8.w),
+            if (isSelected)
+              Text(
+                label,
+                style: TextStyle(
+                  color: AppColors.primary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14.sp,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
