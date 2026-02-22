cf90f107bcedbab229f79ab1175469e38519b1c8
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-22 22:04:03 +0200

    update home cubit to make search filter

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,21 +1,44 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../services/home_api_service.dart';
+import '../models/product_model.dart';
 import 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
 
   final HomeApiService api = HomeApiService(Dio());
+  List<ProductModel> allProducts = [];
 
   Future<void> fetchProducts() async {
     emit(HomeLoading());
-
     try {
-      final products = await api.getProducts();
-      emit(HomeSuccess(products));
+      allProducts = await api.getProducts();
+      emit(HomeSuccess(allProducts));
     } catch (e) {
       emit(HomeError(e.toString()));
+    }
+  }
+
+  void searchProducts(String query) {
+    if (query.isEmpty) {
+      emit(HomeSuccess(allProducts));
+    } else {
+      final filtered = allProducts
+          .where((p) => p.title.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+      emit(HomeSuccess(filtered));
+    }
+  }
+
+  void filterByCategory(String categoryName) {
+    if (categoryName == "Men" || categoryName == "All") {
+      emit(HomeSuccess(allProducts));
+    } else {
+      final filtered = allProducts
+          .where((p) => p.category.toLowerCase() == categoryName.toLowerCase())
+          .toList();
+      emit(HomeSuccess(filtered));
     }
   }
 }
