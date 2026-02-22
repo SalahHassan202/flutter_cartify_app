0fcdabda22b6b229ea9561ef17ac08d7aa472b11
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-23 00:16:18 +0200

    update product card file

--- lib/features/home/widgets/product_card.dart
+++ lib/features/home/widgets/product_card.dart
@@ -1,4 +1,7 @@
+import 'package:cartify_app/features/cart/cubit/cart_cubit.dart';
+import 'package:cartify_app/features/favorites/cubit/favourite_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_text_styles.dart';
@@ -7,20 +10,17 @@ import '../models/product_model.dart';
 
 class ProductCard extends StatelessWidget {
   final ProductModel product;
-
   const ProductCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ProductDetailsScreen(product: product),
-          ),
-        );
-      },
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ProductDetailsScreen(product: product),
+        ),
+      ),
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
@@ -29,12 +29,10 @@ class ProductCard extends StatelessWidget {
             BoxShadow(
               color: Colors.black.withValues(alpha: 0.04),
               blurRadius: 15,
-              offset: const Offset(0, 6),
             ),
           ],
         ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               children: [
@@ -44,15 +42,41 @@ class ProductCard extends StatelessWidget {
                   ),
                   child: Image.network(
                     product.image,
-                    height: 150.h,
+                    height: 140.h,
                     width: double.infinity,
                     fit: BoxFit.cover,
+                  ),
+                ),
+
+                Positioned(
+                  top: 8.h,
+                  right: 8.w,
+                  child: _miniBtn(
+                    icon: context.watch<FavoritesCubit>().isFavorite(product.id)
+                        ? Icons.favorite
+                        : Icons.favorite_border,
+                    color:
+                        context.watch<FavoritesCubit>().isFavorite(product.id)
+                        ? Colors.red
+                        : AppColors.primary,
+                    onTap: () =>
+                        context.read<FavoritesCubit>().toggleFavorite(product),
+                  ),
+                ),
+
+                Positioned(
+                  top: 8.h,
+                  left: 8.w,
+                  child: _miniBtn(
+                    icon: Icons.add_shopping_cart,
+                    color: AppColors.primary,
+                    onTap: () => context.read<CartCubit>().addToCart(product),
                   ),
                 ),
               ],
             ),
             Padding(
-              padding: EdgeInsets.all(10.w),
+              padding: EdgeInsets.all(8.w),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -61,10 +85,9 @@ class ProductCard extends StatelessWidget {
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                     style: AppTextStyles.body.copyWith(
-                      fontWeight: FontWeight.w600,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 6.h),
                   Text(
                     "\$${product.price}",
                     style: AppTextStyles.body.copyWith(
@@ -77,6 +100,25 @@ class ProductCard extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _miniBtn({
+    required IconData icon,
+    required Color color,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(5.w),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 4)],
+        ),
+        child: Icon(icon, size: 18.sp, color: color),
       ),
     );
   }
